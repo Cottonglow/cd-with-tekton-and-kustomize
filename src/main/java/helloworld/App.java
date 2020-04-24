dee5229414588b0c62358b25b5a991e3ccb3802a
Sandra Hayward <cotton_glow@hotmail.com>
2020-04-24 10:49:33 +0100

    Fixed the helloworld application

--- src/main/java/helloworld/App.java
+++ src/main/java/helloworld/App.java
@@ -5,5 +5,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hello " + args[0]);
+        
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
